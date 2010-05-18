c263eb638a02c96010d614fcd0200eeed89bf58e
ayu.devel <ayu.devel@988d8c17-f230-fc3c-89af-c011746e55b5>
2010-05-18 13:22:35 +0000

    asdf

---
+++ tractor/src/tractor/client/game/Button.java
@@ -0,0 +1,14 @@
+package tractor.client.game;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.gui.GUIContext;
+import org.newdawn.slick.gui.MouseOverArea;
+
+public class Button extends MouseOverArea {
+
+	public Button(GUIContext container, Image image, int x, int y) {
+		super(container, image, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+}

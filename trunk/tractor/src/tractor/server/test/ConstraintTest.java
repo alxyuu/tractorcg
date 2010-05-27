de8554b850497d7f37d33719957b7b37d13b4653
ayu.devel <ayu.devel@988d8c17-f230-fc3c-89af-c011746e55b5>
2010-05-27 03:34:10 +0000

    trick

--- trunk/tractor/src/tractor/server/test/ConstraintTest.java
+++ trunk/tractor/src/tractor/server/test/ConstraintTest.java
@@ -3,6 +3,7 @@ package tractor.server.test;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import tractor.lib.Card;
@@ -16,6 +17,7 @@ public class ConstraintTest {
 	private User highest;
 	private int currentSuit;
 	private int TRUMP_SUIT, TRUMP_NUMBER;
+	private Trick currenTrick;
 	
 	public static void main(String[] args) {
 		Card.populateDeck();
@@ -100,8 +102,19 @@ public class ConstraintTest {
 				}
 				suit = ( card.getNumber() == this.TRUMP_NUMBER ) ? Card.TRUMP : card.getSuit();
 			}
-			this.currentSuit = suit;
 			
+			Trick trick = new Trick();
+			//calculate and add plays to the trick
+			
+			//check if high only if there's more than one play
+			if(trick.countPlays() >= 1) {
+				
+				//cock goes here
+				
+			} else if (trick.countPlays() == 0) { // this should never happen...
+				System.out.println("some bad shit happened");
+				return;
+			}
 			
 			/*boolean isHigh=true;
 			int suits=0;
@@ -115,14 +128,24 @@ public class ConstraintTest {
 				if(u != user)
 				{
 					PlayerHand friedchicken = u.getHand();
-					List<Card> seven = friedchicken.getCards();
+					List<Card> seven = friedchicken.getCards(		Iterator <Card>it2=user.getHand().iterator();
+		while(it2.hasNext())
+		{
+			Card c=it2.next();
+			user.getHand().frequency(c);
+		});
 
 				}
 			}*/
 			
 			
+			
+			//the trick is valid so set it
+			this.currenTrick = trick;
 			//assume lead is always high until beaten by someone else
 			this.highest = user;
+			//don't set suit until after the play has been verified as high
+			this.currentSuit = suit;
 		} else {
 			//not lead, check following suit, playing doubles/tractors/triples/whatever
 			//compare to highest user's play
@@ -137,11 +160,59 @@ public class ConstraintTest {
 	}
 }
 
-class Double
+class Trick {
+	private List<Card> singles;
+	private List<Pair> pairs;
+	private List<Triple> triples;
+	private List<Tractor> tractors;
+	Trick() {
+		this.singles = new LinkedList<Card>();
+		this.pairs = new LinkedList<Pair>();
+		this.triples = new LinkedList<Triple>();
+		this.tractors = new LinkedList<Tractor>();
+	}
+	
+	public void addSingle(Card card) {
+		this.singles.add(card);
+	}
+	
+	public void addPair(Pair pair) {
+		this.pairs.add(pair);
+	}
+	
+	public void addTriple(Triple triple) {
+		this.triples.add(triple);
+	}
+	
+	public void addTractor(Tractor tractor) {
+		this.tractors.add(tractor);
+	}
+	
+	public int countSingles() {
+		return this.singles.size();
+	}
+	
+	public int countPairs() {
+		return this.pairs.size();
+	}
+	
+	public int countTriples() {
+		return this.triples.size();
+	}
+	
+	public int countTractors() {
+		return this.tractors.size();
+	}
+	
+	public int countPlays() {
+		return this.countSingles() + this.countPairs() + this.countTriples() + this.countTractors();
+	}
+	
+}
+class Pair
 {
-	int number;
 	Card c;
-	Double(Card card)
+	Pair(Card card)
 	{
 		this.c = card;
 	}
@@ -152,5 +223,25 @@ class Double
 	public int getNumber()
 	{
 		return c.getNumber();
+	}
+}
+
+class Triple extends Pair
+{
+	Triple(Card card) {
+		super(card);
+	}
+}
+
+class Tractor {
+	int pairs;
+	int triples;
+	int start;
+	int end;
+	Tractor(int pairs, int triples, int start, int end) {
+		this.pairs = pairs;
+		this.triples = triples;
+		this.start = start;
+		this.end = end;
 	}
 }

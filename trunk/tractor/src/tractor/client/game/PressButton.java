8cccd485a7583fa7d6531ff22ec532279c0f40f0
ayu.devel@gmail.com <ayu.devel@gmail.com@988d8c17-f230-fc3c-89af-c011746e55b5>
2011-08-03 01:12:23 +0000

    view last trick

--- trunk/tractor/src/tractor/client/game/PressButton.java
+++ trunk/tractor/src/tractor/client/game/PressButton.java
@@ -5,20 +5,24 @@ import org.newdawn.slick.gui.GUIContext;
 
 public class PressButton extends Button {
 	ButtonReleasedListener released;
+	private boolean pressed;
 	public PressButton(GUIContext container, Image normal, Image mouseover, int x, int y) {
 		super(container, normal, mouseover, x, y);
+		this.pressed = false;
 	}
 	public void addButtonReleasedListener(ButtonReleasedListener l) {
 		this.released = l;
 	}
 	public void mouseReleased(int button, int mx, int my) {
-		if(this.released != null && this.isMouseOver()) {
+		if(this.released != null && this.pressed) {
 			this.released.buttonReleased();
 		}
+		this.pressed = false;
 	}
 	public void mousePressed(int button, int mx, int my) {
 		if(this.listener != null && this.isMouseOver()) {
 			this.listener.buttonPressed();
+			this.pressed = true;
 		}
 	}
 }

d31719870ffdb9bc219f510d3628a81db1f165e2
Alex Yu <alex@rigup.com>
2020-03-26 23:09:59 -0500

    hnngg

--- build_client.sh
+++ build_client.sh
@@ -2,6 +2,8 @@
 
 javac -d client_bin/ -cp lib/slick.jar -sourcepath src/main/java src/main/java/tractor/client/JarRunner.java
 cp lib/native/*.zip client_bin/tractor/client/
+unzip lib/slick.jar -o -d client_bin
+unzip lib/lwjgl.jar -o -d client_bin
 cd client_bin
-jar -cvfm client.jar client.manifest tractor/ ../images
+jar -cvfm client.jar ../client.manifest ./ ../images
 cd ..
